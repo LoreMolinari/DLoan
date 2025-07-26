f825ead7db1528fbb198d8fec505a1f6b487df06
Lorenzo Molinari <lore.molinari.00@gmail.com>
2025-07-26 17:07:25 +0200

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,7 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
 
 // Test account with more funds
-const PRIVATE_KEY = "59c6995e998f97a5a0044966f0945389dc9e86dae88c7a8412f4603b6b78690d";
+const PRIVATE_KEY = "@APIKEY";
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
